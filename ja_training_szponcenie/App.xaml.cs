ffa32d7a17110c49a6ea2a286445bc0de3a8abe6
wojtek <wojtek@DESKTOP-MSN5DM6>
2025-11-13 19:36:35 +0100

    Add project files.

---
+++ ja_training_szponcenie/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ja_training_szponcenie
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
